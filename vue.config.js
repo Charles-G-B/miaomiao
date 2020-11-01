4854aa8d8afa4be8f017405eb0fa3485545be0d7
ganbo <GB492617606@163.com>
2020-11-01 15:42:56 +0800

    add setData

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.export = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://39.97.33.178',
+                changeOrigin: true
+            }
+        }
+    }
+}
